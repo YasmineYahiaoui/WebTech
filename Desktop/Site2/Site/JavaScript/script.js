e0507dc34d4afafd8baac76d1375ad00cc627c99
YasmineYahiaoui <Yahiaoui.yasmine11@gmail.com>
2024-04-08 21:38:41 -0400

    j

--- Desktop/Site2/Site/JavaScript/script.js
+++ Desktop/Site2/Site/JavaScript/script.js
@@ -4,7 +4,7 @@ window.onload = function()
 var canvas = document.createElement('canvas');
 canvas.width = 980 ;
 canvas.height= 600 ;
-canvas.style.border = "1 px solid";
+canvas.style.border = " 1px solid";
 /* on rajout appenchild pour l'attacher a notre page html */ 
 document.body.appendChild(canvas);
 }
